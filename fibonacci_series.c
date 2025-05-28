3d3d9119b0c53b48169a7f250d1618d52d21b893
Sultan9463 <s75696643@gmail.com>
2025-05-28 13:35:58 +0100

    Update fibonacci_series.c

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -4,28 +4,25 @@
 int main() {
     int n;
     int a = 0, b = 1, next;
-
     // Demander à l'utilisateur d'entrer un entier
-    printf("Entrez un nombre entier positif : ");
     if (scanf("%d", &n) != 1) {
         return 1; // erreur de lecture
     }
-
+    printf("Fibonacci :");
+    // si la limmie est négatif 
     if (n < 0) {
         printf("Nombre négatif non autorisé.\n");
         return 1;
     }
-
     // Affichage de la suite de Fibonacci
-    while (a < n) {
+    while (a <= n) {
         printf("%d ", a);
-
         // Protection contre le dépassement d'entier
         if (a > INT_MAX - b) {
             printf("\nErreur : dépassement de capacité.\n");
             return 1;
         }
-
+        // calcul du prochain terme
         next = a + b;
         a = b;
         b = next;

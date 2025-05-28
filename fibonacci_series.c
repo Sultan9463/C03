7170be34e6db67bef51516931c587269c5479d52
Sultan9463 <s75696643@gmail.com>
2025-05-28 13:26:51 +0100

    Update fibonacci_series.c

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -1,15 +1,36 @@
 #include <stdio.h>
+#include <limits.h> // pour INT_MAX
 
-int main(){
-  int n, a = 0, b = 1, next;
-scanf("%d", &n);
-printf("\n");
-while ( a <= n) {
-printf ("%d", a);
-next a = b;
-a = b;
-b = next;
-}
-printf("\n");
-return 0;
+int main() {
+    int n;
+    int a = 0, b = 1, next;
+
+    // Demander à l'utilisateur d'entrer un entier
+    printf("Entrez un nombre entier positif : ");
+    if (scanf("%d", &n) != 1) {
+        return 1; // erreur de lecture
+    }
+
+    if (n < 0) {
+        printf("Nombre négatif non autorisé.\n");
+        return 1;
+    }
+
+    // Affichage de la suite de Fibonacci
+    while (a < n) {
+        printf("%d ", a);
+
+        // Protection contre le dépassement d'entier
+        if (a > INT_MAX - b) {
+            printf("\nErreur : dépassement de capacité.\n");
+            return 1;
+        }
+
+        next = a + b;
+        a = b;
+        b = next;
+    }
+
+    printf("\n");
+    return 0;
 }
